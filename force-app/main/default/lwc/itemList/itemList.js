e68ac67495bec7e3131b4baf59426c8627a55580
Tomiris Altay <tomirisaltaj@gmail.com>
2025-09-08 05:20:49 +0500

    CSS added to fix the UI

--- force-app/main/default/lwc/itemList/itemList.js
+++ force-app/main/default/lwc/itemList/itemList.js
@@ -1,45 +1,17 @@
 import { LightningElement, api, track } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ItemList extends LightningElement {
+  @api items = [];
+  @api total = 0;
+  @api page = 1;
+  @api pageSize = 6;
+  @api loading = false;
 
-  @api items = [];      
-  @api total = 0;       
-  @api loading = false;    
-  @api page = 1;     
-  @api pageSize = 20;     
-
-
-  columns = [
-    { label: 'Name', fieldName: 'Name' },
-    { label: 'Family', fieldName: 'Family__c' },
-    { label: 'Type', fieldName: 'Type__c' },
-    { label: 'Price', fieldName: 'Price__c', type: 'currency' },
-    { label: 'Description', fieldName: 'Description__c', wrapText: true },
-
-    { type: 'action', typeAttributes: { rowActions: this.getRowActions } }
-  ];
-
-  getRowActions(row, done) {
-    done([
-      { label: 'Details', name: 'details' },
-      { label: 'Add to Cart', name: 'add_to_cart' }
-    ]);
-  }
-
+  @track isDetailsOpen = false;
+  @track selected = {};
 
   get hasRows() {
-    return Array.isArray(this.items) && this.items.length > 0;
-  }
-
-  get fromNum() {
-    const start = (this.page - 1) * this.pageSize + 1;
-    return this.hasRows ? start : 0;
-  }
-
-  get toNum() {
-    const end = (this.page - 1) * this.pageSize + this.items.length;
-    return this.hasRows ? end : 0;
+    return (this.items && this.items.length > 0);
   }
 
   get disablePrev() {
@@ -50,37 +22,42 @@ export default class ItemList extends LightningElement {
     return this.page * this.pageSize >= this.total;
   }
 
+  get fromNum() {
+    if (!this.total) return 0;
+    return (this.page - 1) * this.pageSize + 1;
+  }
 
-  handlePrev() {
+  get toNum() {
+    return Math.min(this.page * this.pageSize, this.total || 0);
+  }
+
+  handlePrev = () => {
     this.dispatchEvent(new CustomEvent('prev'));
-  }
-  handleNext() {
+  };
+  handleNext = () => {
     this.dispatchEvent(new CustomEvent('next'));
-  }
-
-  // ===== DETAILS MODAL =====
-  @track isDetailsOpen = false;
-  @track selected = null;
-
-  handleRowAction(event) {
-    const { name } = event.detail.action;
-    const row = event.detail.row;
-    if (name === 'details') {
-      this.selected = row;
-      this.isDetailsOpen = true;
-    } else if (name === 'add_to_cart') {
-      this.dispatchEvent(new CustomEvent('addtocart', { detail: { item: row } }));
-    }
-  }
-
-  closeDetails() {
-    this.isDetailsOpen = false;
-    this.selected = null;
-  }
-
-  handleEditSuccess() {
-    this.dispatchEvent(new ShowToastEvent({ title: 'Item saved', variant: 'success' }));
-    this.closeDetails();
+  };
+  handleRefresh = () => {
     this.dispatchEvent(new CustomEvent('refresh'));
-  }
+  };
+
+  handleAdd = (e) => {
+    const id = e.currentTarget.dataset.id;
+    const item = this.items.find(x => x.Id === id) || this.selected;
+    this.dispatchEvent(new CustomEvent('addtocart', { detail: { item } }));
+  };
+
+  openDetails = (e) => {
+    const id = e.currentTarget.dataset.id;
+    const found = this.items.find(x => x.Id === id);
+    if (found) {
+      this.selected = found;
+      this.isDetailsOpen = true;
+    }
+  };
+
+  closeDetails = () => {
+    this.isDetailsOpen = false;
+    this.selected = {};
+  };
 }
